ec69ea87ab461db3224e7f09740d97d7db2ccfbb
HellishPirate <35464378+HellishPirate@users.noreply.github.com>
2022-12-03 18:01:08 +0000

    Add files via upload

---
+++ Day 3/day3.m
@@ -0,0 +1,29 @@
+clear variables
+load day_3_input.mat
+C = char(day3input);
+unicodeValues = double(C);
+unicodeValues(unicodeValues == 32) = NaN;
+
+hex_a_z = 97:122;
+a_z = 1:26;
+unicodeValues = changem(unicodeValues, a_z, hex_a_z);
+
+hex_A_Z = 65:90;
+A_Z = 27:52;
+unicodeValues = changem(unicodeValues, A_Z, hex_A_Z);
+
+total1 = 0;
+for i = 1:300
+    sans_nans = sum(isnan(unicodeValues(i,:)));
+    a1 = unicodeValues(i,1:(end-sans_nans)/2);
+    b1 = unicodeValues(i,((end-sans_nans)/2)+1:end-sans_nans);
+    dupes = intersect(a1,b1);
+    total1 = total1 + dupes;
+end
+
+total2 = 0;
+for j = 3:3:300
+    a1 = unicodeValues(j-2:j,:);
+    dupes1 = intersect(intersect(a1(1,:),a1(2,:)),a1(3,:));
+    total2 = total2 + dupes1;
+end
