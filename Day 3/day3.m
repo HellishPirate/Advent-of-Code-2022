29f6859452995d01bfe34c7fcec8e19a318550b8
HellishPirate <35464378+HellishPirate@users.noreply.github.com>
2022-12-03 21:55:33 +0000

    Update day3.m

--- Day 3/day3.m
+++ Day 3/day3.m
@@ -4,13 +4,13 @@ C = char(day3input);
 unicodeValues = double(C);
 unicodeValues(unicodeValues == 32) = NaN;
 
-hex_a_z = 97:122;
+dec_a_z = 97:122;
 a_z = 1:26;
-unicodeValues = changem(unicodeValues, a_z, hex_a_z);
+unicodeValues = changem(unicodeValues, a_z, dec_a_z);
 
-hex_A_Z = 65:90;
+dec_A_Z = 65:90;
 A_Z = 27:52;
-unicodeValues = changem(unicodeValues, A_Z, hex_A_Z);
+unicodeValues = changem(unicodeValues, A_Z, dec_A_Z);
 
 total1 = 0;
 for i = 1:300
