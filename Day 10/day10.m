ab5f517c2b98bdfe2feecb67a6a3e1b79ddf09bd
HellishPirate <35464378+HellishPirate@users.noreply.github.com>
2022-12-15 21:38:00 +0000

    Update day10.m
    hopefully this will be the final edit

--- Day 10/day10.m
+++ Day 10/day10.m
@@ -11,7 +11,7 @@ count = [1; double(instructions)];
 all_cycles = (0:240)';
 cum_sum_actual = repelem(cum_sum,count);
 cum_sum_actual = [1; cum_sum_actual];
-n = (20:40:240);
+n = 20:40:240;
 
 ans1 = sum(cum_sum_actual(n).*n');
 
@@ -21,7 +21,7 @@ lit_pixels = double(ac2 == cum_sum_actual | ac2 == cum_sum_actual + 1 | ac2 == c
 lit_pixels = lit_pixels(1:end-2);
 
 screen = zeros(6,40);
-n1 = 1:40:280;
+n1 = 1:40:241;
 for i = 1:6
     screen(i,:) = lit_pixels(n1(i):(n1(i+1))-1);
 end
